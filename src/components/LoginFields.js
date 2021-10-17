2424a8e3c569da90c2b52f9366a445d27963e5cc
felipe parra <felipe.parra@magnotechnology.com>
2021-10-16 21:40:48 -0500

    [add] fields to forms

---
+++ src/components/LoginFields.js
@@ -0,0 +1,21 @@
+export const LoginState = {
+  mail: "",
+  password: "",
+};
+
+export const LoginFields = [
+  {
+    id: "mail",
+    name: "mail",
+    type: "email",
+    placeholder: "Type your email",
+    required: true,
+  },
+  {
+    id: "password",
+    name: "password",
+    type: "password",
+    placeholder: "Type your email",
+    required: true,
+  },
+];
---
+++ src/components/SignUpFields.js
@@ -0,0 +1,54 @@
+export const SignUpState = {
+    mail: "",
+    password: "",
+    phone: "",
+    age: 0,
+    gender: "",
+    hobbies: "",
+  };
+  
+  export const SignUpFields = [
+    {
+      id: "name",
+      name: "name",
+      type: "text",
+      placeholder: "Type your name",
+    },
+    {
+      id: "mail",
+      name: "mail",
+      type: "email",
+      placeholder: "Type your email",
+    },
+    {
+      id: "password",
+      name: "password",
+      type: "password",
+      placeholder: "Type your email",
+    },
+    {
+      id: "phone",
+      name: "phone",
+      type: "phone",
+      placeholder: "Type your phone",
+    },
+    {
+      id: "age",
+      name: "age",
+      type: "number",
+      placeholder: "Type your age",
+    },
+    {
+      id: "gender",
+      name: "gender",
+      type: "gender",
+      placeholder: "Type your gender",
+    },
+    {
+      id: "hobbies",
+      name: "hobbies",
+      type: "textarea",
+      placeholder: "Type your hobbies",
+    },
+  ];
+  
